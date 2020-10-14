b2890691ec9dd74bfcd9f34b520b9d5b0a1d6aac
Lewis Denham-Parry <lewis@control-plane.io>
2020-10-14 12:02:13 +0100

    fix: update restify version
    - old version of resitfy causing node to fail when running app
    - updated app to use latest version of restify

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 var ip = require('ip');
 var restify = require('restify');
+
+const server = restify.createServer({
+  name: 'demo-api',
+  version: '0.0.1'
+});
+
+var port = process.env.DEMO_API_PORT || '9000'
+var message = process.env.MESSAGE || 'Hello, world';
 var pkg = require('./package.json');
 
-var server = restify.createServer({ name: 'demo' });
-
-var message = process.env.MESSAGE || 'Hello, world';
-
+server.use(restify.plugins.acceptParser(server.acceptable));
+server.use(restify.plugins.queryParser());
+server.use(restify.plugins.bodyParser());
+ 
 server.get('/', function (req, res, next) {
   res.send(200, `${message} | ${ip.address()} | Version ${pkg.version}`);
   return next();
 });
-
-server.listen(+(process.env.DEMO_API_PORT || '9000'));
+ 
+server.listen(port, function () {
+  console.log('%s listening at %s', server.name, server.url);
+});
